8a3cf20694b3875dd7d6fdadac3f6e94a64740b6
Programmer Abhaya <148797965+programmerabhaya@users.noreply.github.com>
2024-02-10 17:25:24 +0545

    ssss

--- js/link.js
+++ js/link.js
@@ -12,3 +12,24 @@ instagramButton.addEventListener("click", function () {
   // Redirect to the Instagram profile when the Instagram button is clicked
   window.location.href = "https://www.instagram.com/sujitapokhrel112/";
 });
+
+document.getElementById("download-all").addEventListener("click", function () {
+  var assetLinks = document.getElementsByClassName("asset-link");
+  for (var i = 0; i < assetLinks.length; i++) {
+    var link = assetLinks[i];
+    var url = link.getAttribute("href");
+    var fileName = url.substring(url.lastIndexOf("/") + 1);
+    var anchor = document.createElement("a");
+    anchor.style.display = "none";
+    anchor.href = url;
+    anchor.download = fileName;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+  // Show asset links after downloading
+  for (var i = 0; i < assetLinks.length; i++) {
+    assetLinks[i].style.display = "inline"; // Show each asset link
+  }
+  alert("You have sucessfully downloaded images go and visit your gallery");
+});
